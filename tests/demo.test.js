030cac312bda50098a6aa9f8d2edf09f74802689
¨luisalbeto¨ <luis-alberto11@hotmail.com>
2024-06-13 16:33:55 -0400

    Adding jest config files

--- tests/demo.test.js
+++ tests/demo.test.js
@@ -1,7 +1,5 @@
-test('Esta Prueba no debe de fallar', () => {
-
-    if( 1 === 0) {
-        throw new Error('No se puede dividir entre cero')
-    }
-
+describe('Name of the group', () => {
+    test('deberia pasar la prueba',() => {
+        
+    })
 })
